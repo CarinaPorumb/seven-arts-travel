d53551cda94912473d47fca079be64b508641b2f
Carina <carina_porumb@yahoo.com>
2022-08-30 22:53:25 +0300

    update

--- src/main/java/ro/itschool/scheduler/MonthlyScheduler.java
+++ src/main/java/ro/itschool/scheduler/MonthlyScheduler.java
@@ -1,9 +1,9 @@
 package ro.itschool.scheduler;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import ro.itschool.entity.User;
+import ro.itschool.repository.UserRepository;
+import ro.itschool.service.UserService;
 import ro.itschool.service.email.EmailBodyService;
 import ro.itschool.service.email.EmailSender;
 
@@ -12,14 +12,21 @@ public class MonthlyScheduler {
 
     @Autowired
     EmailBodyService emailBodyService;
-
     @Autowired
     EmailSender emailSender;
 
-    @Scheduled(cron = "30 10 ? * 5L")   //At 10:30 a.m. on the last Thursday of every month
-    public void monthlyEmail(User user) {
-        emailSender.sendEmail(user.getEmail(), "Discover the latest art destinations!", emailBodyService.emailBodyScheduler(user));
-    }
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    UserService userService;
+
+//    @Scheduled(cron = "30 10 ? * * 5L")   //At 10:30 a.m. on the last Thursday of every month
+//    public void monthlyEmail() {
+
+//        emailSender.sendEmail(user.getEmail(), "Discover the latest art destinations!", emailBodyService.emailBodyScheduler(user));
+//}
+
 
 }
 
