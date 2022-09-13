c5a3374ae2a7c14b7e9853d3faabed5ae0d5f207
Carina <carina_porumb@yahoo.com>
2022-09-13 15:03:45 +0300

    update

--- src/main/java/ro/itschool/controller/ContactController.java
+++ src/main/java/ro/itschool/controller/ContactController.java
@@ -3,9 +3,7 @@ package ro.itschool.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ro.itschool.entity.Contact;
 import ro.itschool.repository.ContactRepository;
 
@@ -15,17 +13,29 @@ public class ContactController {
     @Autowired
     ContactRepository contactRepository;
 
-    @RequestMapping(value = {"/save-contact"})
-    public String saveContact(Model model) {
-        model.addAttribute("contact", new Contact());
-        return "save-contact";
+    @GetMapping("/contact-message")
+    public String getContact(Model model, String keyword) {
+        model.addAttribute("contacts", contactRepository.searchContact(keyword));
+        return "/contact-message";
     }
 
-    @PostMapping("/save-contact")
+    @GetMapping("/contact")
+    public String saveContact(Model model) {
+        model.addAttribute("contactObject", new Contact());
+       return "contact";
+    }
+
+    @PostMapping("/contact")
     public String saveContact2(@ModelAttribute Contact contact, Model model) {
-        model.addAttribute("contact", contact);
+        model.addAttribute("contactObject", contact);
         contactRepository.save(contact);
-        return "redirect:/all-contacts";
+        return "redirect:/contact";
+    }
+
+    @RequestMapping(path = "/delete-contact/{id}")
+    public String deleteContact(@PathVariable("id") Integer id) {
+        contactRepository.deleteById(id);
+        return "redirect:/contact-message";
     }
 
 }
