e477d57e3a156aa6d127afd39efb5cd148ecd207
CarinaPorumb <carina_porumb@yahoo.com>
2024-03-04 20:24:29 +0200

    update to Java 21 & Spring Boot 3.2.3

--- src/main/java/ro/project/config/WebSecurityConfig.java
+++ src/main/java/ro/project/config/WebSecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,23 +18,23 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authenticationProvider(authenticationProvider())
-                .csrf().disable().authorizeHttpRequests()
-                .requestMatchers("/static/**","/webjars/**", "/users/postman", "/login", "/forgot-password", "/register", "/activation/**", "/activation-success").permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .failureUrl("/login-error")
-                .defaultSuccessUrl("/index", true)
-                .and()
-                .logout()
-                .logoutSuccessUrl("/login")
-                .and()
-                .sessionManagement()
-                .maximumSessions(1);
-        http.headers().frameOptions().sameOrigin();
+
+        http.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/static/**", "/webjars/**", "/users/postman", "/login", "/forgot-password", "/register", "/activation/**", "/activation-success").permitAll()
+                        .anyRequest().authenticated())
+
+                .formLogin(login -> login
+                        .loginPage("/login")
+                        .failureUrl("/login-error")
+                        .defaultSuccessUrl("/index", true))
+
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/login"))
+
+                .sessionManagement(session -> session.maximumSessions(1))
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(headers -> headers
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
         return http.build();
     }
 
@@ -53,5 +55,4 @@ public class WebSecurityConfig {
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
 }
